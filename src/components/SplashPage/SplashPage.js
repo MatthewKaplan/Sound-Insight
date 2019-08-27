387d2cc9442ab2a534b57f20ab81d0892badf0a2
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-26 20:43:44 -0600

    Splashpage styling

---
+++ src/components/SplashPage/SplashPage.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './SplashPage.scss';
+
+class SplashPage extends Component {
+	render () {
+		return (
+			<div className="splash-page">
+				<div className="color-overlay" />
+				<img
+					src={require(`../../assets/images/${this.props.image.toLowerCase()}`)}
+					className="splash-img"
+					alt={this.props.altText}
+				/>
+				<div className="splash-desc">
+					<p>{this.props.subtitle}</p>
+					<p>
+						<strong>{this.props.title}</strong>
+					</p>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default SplashPage;
