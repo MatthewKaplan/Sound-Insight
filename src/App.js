c81bd3a5ba0f96ab7dd7d5ddba1c03cae9b8cb72
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-26 20:36:37 -0600

    Add routes for browser router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import ServicesPage from './components/ServicesPage/ServicesPage'
+import Footer from './components/Footer/Footer';
+import { Route, Switch } from 'react-router-dom';
+import ProductsPage from './components/ProductsPage/ProductsPage';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Sound Insight</h1>
-    </div>
-  );
+function App (){
+	return (
+		<div className="App">
+			<Header />
+			<Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/services" component={ServicesPage} />
+				<Route path="/products" component={ProductsPage} />
+			</Switch>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
