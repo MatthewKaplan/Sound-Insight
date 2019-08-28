2b346926361fb0705d39e287e84c7f6889d2006a
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-28 16:06:57 -0600

    Add header for brand logos

--- src/components/ProductsPage/ProductsPage.js
+++ src/components/ProductsPage/ProductsPage.js
@@ -13,13 +13,25 @@ class ProductsPage extends Component {
 
 	render () {
 		return (
-			<div>
+			<div className="products-page">
 				<SplashPage
 					image="store.jpeg"
 					altText="Living room"
 					subtitle="We carry top of the line brands guarenteed to excite the senses."
 					title="PRODUCTS"
 				/>
+				<div className="brand-heading">
+					<h2>
+						<strong>Featured Brands</strong>
+					</h2>
+					<h2 className="brand-subtitle">
+						Below is a list of some of our top manufacturers. If you can't find what you've been searching for,{' '}
+						<a href="mailto:sales@sound-insight.com">please contact us.</a>
+					</h2>
+				</div>
+				<div className="hr-line">
+					<hr className="page-break" />
+				</div>
 				<section className="logo-container">{this.renderLogos()}</section>
 			</div>
 		);
