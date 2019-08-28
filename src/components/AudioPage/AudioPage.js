d2d0cc1785e4f3c4675e2bd9344d06916a29375b
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-27 20:00:50 -0600

    Create page for companies audio equipment/services

---
+++ src/components/AudioPage/AudioPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './AudioPage.scss';
+
+class AudioPage extends Component {
+	render () {
+		return (
+			<div className="audio-page">
+				<div className="header" />
+				<h1>AUDIO</h1>
+			</div>
+		);
+	}
+}
+
+export default AudioPage;
