c0e6f3e8072d055b1b7a25e163b1a551becd465e
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-27 20:00:28 -0600

    Create about component that will describe company

---
+++ src/components/AboutPage/AboutPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './AboutPage.scss';
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <div className="about-page">
+        <div className="header" />
+        <h1>ABOUT</h1>
+      </div>
+    )
+  }
+}
+
+export default AboutPage;
