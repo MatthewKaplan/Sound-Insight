a840d27a5403acc15fdffc320922bf7b21425a02
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-27 20:01:27 -0600

    Create contact page to reach company

---
+++ src/components/ContactPage/ContactPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './ContactPage.scss';
+
+class ContactPage extends Component {
+	render () {
+		return (
+			<div className="contact-page">
+				<div className="header" />
+				<h1>CONTACT</h1>
+			</div>
+		);
+	}
+}
+
+export default ContactPage;
