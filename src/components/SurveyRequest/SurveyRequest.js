97ffb9c30ce0d202d0428530c3eba62c97c7ef17
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-28 16:06:36 -0600

    Finished survey form styling and popup feature

---
+++ src/components/SurveyRequest/SurveyRequest.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import './SurveyRequest.scss';
+
+class SurveyRequest extends Component {
+	state = { phone: '', firstName: '', lastName: '', email: '', jobDetail: '', dateRequested: '' };
+
+	render () {
+		return (
+			<div className="survey-request-form">
+				<div className="form-information">
+					<img
+						src="https://i.imgur.com/FvyNCGT.png"
+						alt="close button"
+						className="close-form"
+						onClick={() => this.props.closeForm()}
+					/>
+					<h1>Request Free Estimate</h1>
+					<h3 className="name-label">Name*</h3>
+					<div className="name-forms">
+						<section className="fist-name-form">
+							<input type="text" className="first-name form-input" /> <br />
+							<label className="name-label">First Name</label>
+						</section>
+						<section className="last-name-form">
+							<input type="text" className="last-name form-input" /> <br />
+							<label className="name-label">Last Name</label>
+						</section>
+					</div>
+					<div className="desired-demo">
+						<h3>Job Detail*</h3>
+						<textarea
+							name=""
+							id=""
+							cols="60"
+							rows="10"
+							placeholder="In as much detail as possible, tell us about your job."
+						/>
+					</div>
+					<div className="requested-date">
+						<h3>Date Requested*</h3>
+						<div className="date-input">
+							<input type="date" id="start" name="trip-start" min="2019-09-01" max="2022-12-31" className="date-form" />
+						</div>
+					</div>
+					<div className="email">
+						<h3>Email Address*</h3>
+						<input type="text" className="email-address form-input" required />
+					</div>
+					<div className="phone-number">
+						<h3>Phone*</h3>
+						<PhoneInput
+							placeholder="Enter phone number"
+							value={this.state.phone}
+							onChange={phone => this.setState({ phone })}
+							country="US"
+							className="phone-input"
+						/>
+					</div>
+					<input type="submit" value="Submit" className="submit-btn" />
+				</div>
+			</div>
+		);
+	}
+}
+
+export default SurveyRequest;
