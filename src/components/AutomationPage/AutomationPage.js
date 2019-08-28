192d03d445e0fd533dc82d7d596b64f6f833a170
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-27 20:01:10 -0600

    Create page for company automation equip/services

---
+++ src/components/AutomationPage/AutomationPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './AutomationPage.scss';
+
+class AutomationPage extends Component {
+	render () {
+		return (
+			<div className="automation-page">
+				<div className="header" />
+				<h1>AUTOMATION</h1>
+			</div>
+		);
+	}
+}
+
+export default AutomationPage;
