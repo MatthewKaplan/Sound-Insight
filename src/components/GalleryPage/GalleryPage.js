875022e55b8f3eb71ed03a915fa28da7bf16448c
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-27 20:02:26 -0600

    Create component for company to post photos

---
+++ src/components/GalleryPage/GalleryPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './GalleryPage.scss';
+
+class GalleryPage extends Component {
+	render () {
+		return (
+			<div className="gallery-page">
+				<div className="header" />
+				<h1>GALLERY</h1>
+			</div>
+		);
+	}
+}
+
+export default GalleryPage;
