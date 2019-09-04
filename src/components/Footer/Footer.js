bb2959c91d84811a549cf6699777c501899c7be6
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-09-03 20:46:14 -0600

    Change map size and fix footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,29 +6,32 @@ class Footer extends Component {
 		return (
 			<div className="footer-component">
 				<div className="footer-info">
-					<section className="mapouter">
-						<div className="gmap_canvas">
+					<div class="mapouter">
+						<div class="gmap_canvas">
 							<iframe
-								width="650"
+								width="550"
 								height="350"
 								id="gmap_canvas"
-								src="https://maps.google.com/maps?q=382%20New%20York%20Ave%2C%20Huntington%2C%20NY%2011743&t=&z=15&ie=UTF8&iwloc=&output=embed"
-								frameBorder="0"
+								src="https://maps.google.com/maps?q=382%20New%20York%20Ave%20Huntington%2C%20NY%2011743&t=&z=15&ie=UTF8&iwloc=&output=embed"
+								frameborder="0"
 								scrolling="no"
-								marginHeight="0"
-								marginWidth="0"
-							/>Google Maps Generator by <a href="https://www.embedgooglemap.net">embedgooglemap.net</a>
+								marginheight="0"
+								marginwidth="0"
+							/>
+							<a href="https://www.utilitysavingexpert.com">Utility Saving Expert</a>
 						</div>
-					</section>
+					</div>
 					<section className="store-info">
-						<h3>
-							<a target="_blank" href="tel:6312714434" rel="noopener noreferrer">
-								631.271.4434
-							</a>
-						</h3>
-						<h3 className="email">
-							<a href="mailto:sales@sound-insight.com">sales@sound-insight.com</a>
-						</h3>
+						<div className="store-contact-info">
+							<h3>
+								<a target="_blank" href="tel:6312714434" rel="noopener noreferrer">
+									631.271.4434
+								</a>
+							</h3>
+							<h3 className="email">
+								<a href="mailto:sales@sound-insight.com">sales@sound-insight.com</a>
+							</h3>
+						</div>
 						<section className="store-information">
 							<section className="store-address">
 								<h3>
