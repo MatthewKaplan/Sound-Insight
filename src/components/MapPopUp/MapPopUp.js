b6e4e7ac3e1db9cf114057fd180f94ebf17eac9f
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-09-04 00:57:33 -0600

    Fix map icon

--- src/components/MapPopUp/MapPopUp.js
+++ src/components/MapPopUp/MapPopUp.js
@@ -5,7 +5,8 @@ class MapPopUp extends Component {
 	render () {
 		return (
 			<div className="map-popup">
-      <img src="https://i.imgur.com/nA0azY8.png" alt="" className="close-popup" onClick={this.props.closeMap} />
+      <img src="https://i.imgur.com/6BxSjmk.png" alt="" className="close-popup" onClick={this.props.closeMap} />
+      {/*<img src="https://i.imgur.com/nA0azY8.png" alt="" className="close-popup" onClick={this.props.closeMap} />*/}
 				<section className="map">
 					<div className="mapouter">
 						<div className="gmap_canvas">
